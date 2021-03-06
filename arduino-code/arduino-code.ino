a17a318b4b753fc14276539c19fe41a6c1d9d238
Amin Asadi <aminasadi329@gmail.com>
2021-03-06 17:57:38 +0330

    all buttons work

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,19 +1,22 @@
+#define SPEED_CHANGE_VALUE 10
+
 int in1 = 11;
 int in2 = 10;
 int en = 9;
 
-int stop_btn = 0;
+int pause_btn = 0;
 int v_inc_btn = 1;
 int v_dec_btn = 2;
 int chdir_btn = 3;
 
 boolean motor_dir = 0;
 int motor_speed = 0;
+int is_paused = 0;
 
 
 void setup() {
   // Set all the motor control pins to outputs
-  pinMode(stop_btn, INPUT_PULLUP);
+  pinMode(pause_btn, INPUT_PULLUP);
   pinMode(v_inc_btn, INPUT_PULLUP);
   pinMode(v_dec_btn, INPUT_PULLUP);
   pinMode(chdir_btn, INPUT_PULLUP);
@@ -25,12 +28,18 @@ void setup() {
   // Turn off motors - Initial state
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  analogWrite(en, 100);
+  analogWrite(en, motor_speed);
 }
 
 void loop() {
-  if(!digitalRead(stop_btn)){                // If direction button is pressed
-    while(!digitalRead(stop_btn));           // Wait until direction button released
+  control_direction();
+  control_speed();
+  control_pause();
+}
+
+void control_direction(){
+    if(!digitalRead(chdir_btn)){                // If direction button is pressed
+    while(!digitalRead(chdir_btn));           // Wait until direction button released
     motor_dir = !motor_dir;                // Toggle direction variable
     if(motor_dir){
         digitalWrite(in1, LOW);
@@ -39,6 +48,32 @@ void loop() {
     else {
       digitalWrite(in1, HIGH);
       digitalWrite(in2, LOW); 
+    }
+  }
+}
+
+void control_speed(){
+  if(!digitalRead(v_inc_btn)){
+    while(!digitalRead(v_inc_btn));
+    motor_speed += SPEED_CHANGE_VALUE;
+    analogWrite(en, motor_speed);
+  }
+  if(!digitalRead(v_dec_btn)){
+    motor_speed -= SPEED_CHANGE_VALUE;
+    analogWrite(en, motor_speed);
+  }
+}
+
+void control_pause(){
+  if(!digitalRead(pause_btn)){
+    while(!digitalRead(pause_btn));
+    if(is_paused){
+      analogWrite(en, motor_speed);
+      is_paused = 0;
+    }
+    else{
+      analogWrite(en, 0);
+      is_paused = 1;
     }
   }
 }
